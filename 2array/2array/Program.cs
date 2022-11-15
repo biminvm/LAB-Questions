0eed49edea91988d2cbb34e483de13fc875a9876
bimin.vm <biminvm27@gmail.com>
2022-11-15 13:11:05 +0530

    2nd commit

--- 2array/2array/Program.cs
+++ 2array/2array/Program.cs
@@ -1,10 +1,26 @@
 ﻿
 int[] fare = new int[3];
+int i, pos;
 
 string[] des = new string[3];
-Console.WriteLine("Input 10 names in the array ");
-for (i = 0; i < names.Length; i++)
+
+for (i = 0; i < fare.Length; i++)
 {
-    Console.Write($"Name  {+i + 1} : ");
-    names[i] = (Console.ReadLine());
+    Console.Write($"enter  the fare  {+i + 1} : ");
+    fare[i] = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write($"enter  the Destination  {+i + 1} : ");
+    des[i] =Console.ReadLine();
+
+}
+
+Console.WriteLine("enter the position :");
+pos=Convert.ToInt32(Console.ReadLine());
+
+for (i = 0; i < fare.Length; i++)
+{
+    if(i==pos-1)
+    {
+        Console.WriteLine( $" The fare is {fare[i]} and the destination is {des[i]}");
+    }
 }
---
+++ merge _array/merge _array/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+string[] names = new string[3]; int i;
+string[] des = new string[3];
+
+
+for (i = 0; i < names.Length; i++)
+{
+    Console.Write($"Name  {+i + 1} : ");
+    names[i] = (Console.ReadLine());
+
+   Console.Write($"Destination  {+i + 1} : ");
+    des[i] = (Console.ReadLine());
+}
+
+
+Console.WriteLine("Names....");
+        foreach (var ele in names)
+        {
+            Console.WriteLine(ele);
+        }
+      
+        Console.WriteLine("Destinations....");
+        foreach (string ele in des)
+        {
+            Console.WriteLine(ele);
+        }
+        var myList = new List<string>();
+        myList.AddRange(names);
+        myList.AddRange(des);
+        string[] arr3 = myList.ToArray();
+        Console.WriteLine("Combined array elements..");
+        foreach (string res in arr3)
+        {
+            Console.WriteLine(res);
+        }
